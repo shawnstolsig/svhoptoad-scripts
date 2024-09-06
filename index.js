537e1db4f6fdfc94c91cf313de546a9b56416a1d
Shawn Stolsig <shawnstolsig@gmail.com>
2024-09-05 18:32:04 -0700

    populates new updatedAt field each time a post is updated

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ const sanity = sanityClient({
  * @param allLocations
  * @returns {{lng, lat, timestamp}}
  */
-function findClosestLocation(timestamp, allLocations){
+function findClosestLocation(timestamp, allLocations) {
     const epoch = timestamp.getTime() / 1000
     const flattenedLocations = allLocations.flat(Infinity);
-    for(let i = 1; i < flattenedLocations.length; i++){
-        if(flattenedLocations[i].t >= epoch){
+    for (let i = 1; i < flattenedLocations.length; i++) {
+        if (flattenedLocations[i].t >= epoch) {
             const {
                 t,
                 p: {
@@ -55,7 +55,7 @@ function findClosestLocation(timestamp, allLocations){
  * @param raw
  * @returns {{text: string, photos: *[]}}
  */
-function parseRawMessage(raw){
+function parseRawMessage(raw) {
     // cleanup message, pull out any photos into array
     const photoRegex = /!\[Photo\|\d+x\d+]\((.+)\)/g;
     const photoMatches = raw.match(photoRegex)
@@ -63,7 +63,7 @@ function parseRawMessage(raw){
     let photos = []
 
     // if message contains photos
-    if(photoMatches){
+    if (photoMatches) {
 
         // parse out photo urls from photo string
         photos = photoMatches.map(photo => {
@@ -76,22 +76,22 @@ function parseRawMessage(raw){
 
             // find the height and width
             const dimensions = photo.match(dimensionsRegex)
-            if(!dimensions) return null
+            if (!dimensions) return null
             const heightAndWidth = dimensions[0].match(dimensionRegex)
-            if(!heightAndWidth) return null
+            if (!heightAndWidth) return null
             const width = Number(heightAndWidth[0])
             const height = Number(heightAndWidth[1])
 
             // find the url
             const urls = photo.match(urlRegex)
-            if(!urls) return null
+            if (!urls) return null
             const url = urls[0].substring(0, urls[0].length - 1)    // chop off trailing ')'
 
             // find the filename
             const filenames = url.match(filenameRegex)
-            if(!filenames) return null
+            if (!filenames) return null
             return {
-                id: filenames[0].replace('.jpeg','').replace('.jpg','').replace('.png',''),
+                id: filenames[0].replace('.jpeg', '').replace('.jpg', '').replace('.png', ''),
                 src: url,
                 height,
                 width,
@@ -112,58 +112,58 @@ function parseRawMessage(raw){
  * @param allLocations
  * @returns {Promise<unknown>}
  */
-async function storeBlogPosts(allBlogPosts, allLocations){
+async function storeBlogPosts(allBlogPosts, allLocations) {
     return new Promise(async (resolve, reject) => {
 
         // diff posts in sanity vs posts in predict wind, filter down to just new posts (minimizes sanity API calls)
         const fetchedBlogPosts = await sanity.fetch(`
         *[_type == 'post']{_id}
         `)
-        const storedBlogPostIds = fetchedBlogPosts.map(({_id}) => _id)
-        const newBlogPosts = allBlogPosts.filter(({topic_id}) => !storedBlogPostIds.includes(topic_id.toString()))
+        const storedBlogPostIds = fetchedBlogPosts.map(({ _id }) => _id)
+        const newBlogPosts = allBlogPosts.filter(({ topic_id }) => !storedBlogPostIds.includes(topic_id.toString()))
 
         // iterate through blog posts, create new blog posts if they don't exist
-        const promises = newBlogPosts.map(async ({topic_id, raw, title, created_at, cooked}) => {
+        const promises = newBlogPosts.map(async ({ topic_id, raw, title, created_at, cooked }) => {
 
             // add 100 ms delay to prevent overloading Sanity api
             await new Promise(res => setTimeout(res, 100));
 
             const { text, photos } = parseRawMessage(raw)
-                const id = topic_id.toString()
+            const id = topic_id.toString()
 
-                const location = findClosestLocation(new Date(created_at), allLocations)
+            const location = findClosestLocation(new Date(created_at), allLocations)
 
-                const doc = {
-                    _type: 'post',
-                    _id: id,
-                    id,
-                    title,
-                    date: created_at,
-                    type: 'Satellite Update',
-                    content: text,
-                    htmlContent: cooked,
-                    location: location ? location : null,
-                }
+            const doc = {
+                _type: 'post',
+                _id: id,
+                id,
+                title,
+                date: created_at,
+                type: 'Satellite Update',
+                content: text,
+                htmlContent: cooked,
+                location: location ? location : null,
+            }
 
-                return sanity.createIfNotExists(doc).then(async postRes => {
-                    console.log(`Post ${postRes._id} was created (or was already present).`)
+            return sanity.createIfNotExists(doc).then(async postRes => {
+                console.log(`Post ${postRes._id} was created (or was already present).`)
 
-                    for(const photo of photos){
-                        const doc = {
-                            _type: 'photo',
-                            _id: photo.id,
-                            post: {
-                                _type: 'reference',
-                                _ref: id
-                            },
-                            location: location ? location : null,
-                            ...photo
-                        }
-                        const photoRes = await sanity.createIfNotExists(doc)
-                        console.log(`Photo ${photoRes._id} was created (or was already present).`)
+                for (const photo of photos) {
+                    const doc = {
+                        _type: 'photo',
+                        _id: photo.id,
+                        post: {
+                            _type: 'reference',
+                            _ref: id
+                        },
+                        location: location ? location : null,
+                        ...photo
                     }
-                })
+                    const photoRes = await sanity.createIfNotExists(doc)
+                    console.log(`Photo ${photoRes._id} was created (or was already present).`)
+                }
             })
+        })
 
         await Promise.all(promises)
         resolve(`Completed storing ${allBlogPosts.length} blog posts.`)
@@ -178,7 +178,7 @@ async function storeBlogPosts(allBlogPosts, allLocations){
  * @param timestamp
  * @returns {Promise<MessageInstance>}
  */
-async function sendSms(toNumber, message, title, timestamp){
+async function sendSms(toNumber, message, title, timestamp) {
 
     // cleanup message, pull out any photos into array
     const photoRegex = /!\[Photo\|\d+x\d+]\((.+)\)/g;
@@ -192,18 +192,18 @@ async function sendSms(toNumber, message, title, timestamp){
     let formattedMsg = `${firstLine} PST: ${title}\n\n${strippedMsg}`
 
     // handle messages longer than Twilio's 1600 character limit
-    if(formattedMsg.length > 1600){
+    if (formattedMsg.length > 1600) {
         formattedMsg = `${firstLine} PST: ${title}\n\nRead it here: www.svhoptoad.com/blog`
     }
 
     // if message contains photos
-    if(photoMatches){
+    if (photoMatches) {
 
         // parse out photo urls from photo string
         const photoUrls = photoMatches.map(photo => {
             const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
             const url = photo.match(urlRegex)
-            if(!url) return null                           // return empty element if no url found
+            if (!url) return null                           // return empty element if no url found
             return url[0].substring(0, url[0].length - 1)  // chop off trailing ')'
         })
 
@@ -218,21 +218,21 @@ async function sendSms(toNumber, message, title, timestamp){
 
     // if no media, send regular sms
     return smsClient.messages.create({
-            body: formattedMsg,
-            from: twilioFromPhone,
-            to: `+1${toNumber}`
-        })
+        body: formattedMsg,
+        from: twilioFromPhone,
+        to: `+1${toNumber}`
+    })
 
 }
 
 /**
  * Fetches locations and blog posts from PredictWind
  */
-function fetchPredictWindData(){
+function fetchPredictWindData() {
     return Promise.all([
-            axios.get('https://forecast.predictwind.com/vodafone/Hoptoad.json?_=1631335739843'),
-            axios.get(' https://forecast.predictwind.com/tracking/blog/Hoptoad?_=1631335739842')
-        ])
+        axios.get('https://forecast.predictwind.com/vodafone/Hoptoad.json?_=1631335739843'),
+        axios.get(' https://forecast.predictwind.com/tracking/blog/Hoptoad?_=1631335739842')
+    ])
 }
 
 /**
@@ -252,19 +252,19 @@ async function updateSanityPosts() {
     ] = await fetchPredictWindData()
 
     // find posts to update by comparing: PW.title + SAN.title and PW.cooked + SAN.htmlContent
-    const updates = pwBlogPosts.filter(({topic_id, title, cooked}) => {
+    const updates = pwBlogPosts.filter(({ topic_id, title, cooked }) => {
 
         // find stored version from Sanity
-        const stored = storedPosts.find(({_id}) => _id === topic_id.toString())
+        const stored = storedPosts.find(({ _id }) => _id === topic_id.toString())
 
         // if post isn't stored, omit from updates
-        if(!stored) {
+        if (!stored) {
             console.log(`couldn't find ${title}, omitting`)
             return false;
         }
 
         // if title and html content are the same, omit from updates
-        if(cooked === stored.htmlContent && title === stored.title) {
+        if (cooked === stored.htmlContent && title === stored.title) {
             console.log(`post ${title} has title match and content match, omitting`)
             return false;
         }
@@ -277,16 +277,16 @@ async function updateSanityPosts() {
 
     // delete existing photos
     await Promise.all(
-        updates.map(({topic_id}) => sanity.delete({ query: `*[_type == 'photo' && post._ref == "${topic_id}"]` }))
+        updates.map(({ topic_id }) => sanity.delete({ query: `*[_type == 'photo' && post._ref == "${topic_id}"]` }))
     )
 
     // delete existing versions
-    const updateIds = updates.map(({topic_id}) => topic_id.toString())
+    const updateIds = updates.map(({ topic_id }) => topic_id.toString())
     await sanity.delete({ query: `*[_type == 'post' && _id in ${JSON.stringify(updateIds)}]` })
 
     // recreate blog posts
     await Promise.all(
-        updates.map(async ({topic_id, raw, title, created_at, cooked}) => {
+        updates.map(async ({ topic_id, raw, title, created_at, cooked }) => {
 
             // add 100 ms delay to prevent overloading Sanity api
             await new Promise(res => setTimeout(res, 100));
@@ -306,12 +306,13 @@ async function updateSanityPosts() {
                 content: text,
                 htmlContent: cooked,
                 location: location ? location : null,
+                updatedAt: new Date()
             }
 
             return sanity.createIfNotExists(doc).then(async postRes => {
                 console.log(`Post ${postRes._id} was updated.`)
 
-                for(const photo of photos){
+                for (const photo of photos) {
                     const doc = {
                         _type: 'photo',
                         _id: photo.id,
@@ -336,7 +337,7 @@ async function updateSanityPosts() {
  * uses Predict Wind API to pull blog posts and photos, storing them to Sanity and Google Sheets,
  * and texting out new blog posts via Twilio
  **/
-async function main(){
+async function main() {
 
     // setup Google Sheet
     await spreadsheet.useServiceAccountAuth({
@@ -349,8 +350,8 @@ async function main(){
     const locationsSheet = await spreadsheet.sheetsByTitle['Locations']
     await locationsSheet.loadCells('A:A')
     let locationIds = []
-    for(let i = 0; i < locationsSheet.rowCount; i++){
-        locationIds.push(locationsSheet.getCell(i,0).value)
+    for (let i = 0; i < locationsSheet.rowCount; i++) {
+        locationIds.push(locationsSheet.getCell(i, 0).value)
     }
     locationIds = [...new Set(locationIds)].filter(cell => cell)
 
@@ -358,8 +359,8 @@ async function main(){
     const blogSheet = await spreadsheet.sheetsByTitle['Blog Posts']
     await blogSheet.loadCells('A:A')
     let blogIds = []
-    for(let i = 0; i < blogSheet.rowCount; i++){
-        blogIds.push(blogSheet.getCell(i,0).value)
+    for (let i = 0; i < blogSheet.rowCount; i++) {
+        blogIds.push(blogSheet.getCell(i, 0).value)
     }
     blogIds = [...new Set(blogIds)].filter(cell => cell)
 
@@ -373,11 +374,11 @@ async function main(){
     await storeBlogPosts(existingBlogPosts, existingLocations)
 
     // filter to only new IDs
-    const newLocations = existingLocations.filter(({t}) => !locationIds.includes(t))
-    const newBlogPosts = existingBlogPosts.filter(({topic_id}) => !blogIds.includes(topic_id))
+    const newLocations = existingLocations.filter(({ t }) => !locationIds.includes(t))
+    const newBlogPosts = existingBlogPosts.filter(({ topic_id }) => !blogIds.includes(topic_id))
 
     // cleanup data before writing to sheet
-    const newLocationsConditioned =  newLocations.map(({t,p,bearing,bsp,twa,twd,tws,gust,isSample}) => ({
+    const newLocationsConditioned = newLocations.map(({ t, p, bearing, bsp, twa, twd, tws, gust, isSample }) => ({
         time: t,
         latitude: p.lat,
         longitude: p.lon,
@@ -391,7 +392,7 @@ async function main(){
     }))
 
     // add new locations to spreadsheet, if there are any
-    if(newLocationsConditioned.length){
+    if (newLocationsConditioned.length) {
         await locationsSheet.addRows(newLocationsConditioned)
     }
 
@@ -406,7 +407,7 @@ async function main(){
 
     // add new blog posts to spreadsheet, if there are any
     // let smsToSend = []
-    if(newBlogPosts.length){
+    if (newBlogPosts.length) {
         await blogSheet.addRows(newBlogPosts)
 
         // send blog posts over sms
